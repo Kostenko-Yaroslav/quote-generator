f66a2a631d73c2678893bcce0eb25e1f8c70dfb6
Kostenko-Yaroslav <yaroslavkostenko34@gmail.com>
2024-08-06 11:58:33 +0300

    Add design to the application

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import "./generator.css";
+import { TbCopy } from "react-icons/tb";
+import { TiSocialTwitter } from "react-icons/ti";
 
 export default function Generator() {
-  return <div>Generator</div>;
+  return (
+    <main>
+      <div className="container">
+        <h1>Quote of the day</h1>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          neque saepe ipsa assumenda nostrum consequatur totam expedita rerum
+          minus ratione? Autem, facilis dolor! Molestias deleniti asperiores ex
+          tenetur est officia.
+        </span>
+        <span className="author">--- author</span>
+        <hr />
+        <footer>
+          <div>
+            <TbCopy size={40} color="#0000fe" className="icon" />
+            <a href="https://twitter.com" target="blank">
+              <TiSocialTwitter size={40} color="#0000fe" className="icon" />
+            </a>
+          </div>
+          <button>New Quote</button>
+        </footer>
+      </div>
+    </main>
+  );
 }
